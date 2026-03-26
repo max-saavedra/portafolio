48c155f4e1e2ec929e3e28cd932142e966b338df
Max <max.bruno.saavedra.2002@gmail.com>
2026-03-25 23:02:39 -0500

    dist v2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   base: "/portafolio/", // ← ESTE ES EL FIX CLAVE
+  publicDir: 'public',
   resolve: {
     alias: {
       "@": "/src",
